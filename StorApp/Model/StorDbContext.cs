9322de15a379f58d9cea446895726e04be453072
Abdullah Jbero <Abdullahjbere@gmail.com>
2022-10-16 18:01:53 +0300

    LogTo

--- StorApp/Model/StorDbContext.cs
+++ StorApp/Model/StorDbContext.cs
@@ -12,5 +12,12 @@ namespace StorApp.Model
 
         public virtual DbSet<Product> Products { get; set; } = null!;
         public virtual DbSet<Brand> Brands { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.LogTo(log => Console.WriteLine(log),
+                new[] {DbLoggerCategory.Database.Command.Name},
+                LogLevel.Information);
+        }
     }
 }
